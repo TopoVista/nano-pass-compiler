0e47d4b04c443cd405fa991122d7f6c4c4ec4b81
souradeep <souradeepghosh608@gmail.com>
2025-12-12 01:36:03 +0530

    Initial commit

---
+++ src/demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "World";
+    std::cout << "Hello, " << name << "!" << std::endl;
+    
+    int sum = 5 + 3;
+    std::cout << "Sum: " << sum << std::endl;
+    
+    return 0;
+}
